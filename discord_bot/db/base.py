43197c50377e53278bb81c19f2b3ad566dcec74b
tesence <thomasesence@gmail.com>
2018-09-25 20:25:12 +0200

    Get rid of "run_until_complete"
    Using "run_until_complete" when the loop is already running and
    some tasks are active, a system of task/event is used instead

--- discord_bot/db/base.py
+++ discord_bot/db/base.py
@@ -1,3 +1,4 @@
+import asyncio
 import functools
 import logging
 
@@ -5,7 +6,7 @@ import asyncpg
 from asyncpg import exceptions
 
 from discord_bot import cfg
-from discord_bot import log
+
 
 CONF = cfg.CONF
 LOG = logging.getLogger('bot')
@@ -48,6 +49,7 @@ def transaction():
     def wrapper(func):
         @functools.wraps(func)
         async def wrapped(*args, **kwargs):
+            await args[0].ready.wait()
             connection = await args[0].pool.acquire()
             async with connection.transaction():
                 result = await func(*args, **kwargs)
@@ -59,20 +61,22 @@ def transaction():
 
 class DBDriver:
 
-    pool = None
-
     def __init__(self, loop, model):
         self.model = model
         self.loop = loop
-        if not DBDriver.pool:
-            DBDriver.pool = loop.run_until_complete(asyncpg.create_pool(**CONF.DATABASE_CREDENTIALS))
-
-        self._get_table_info()
-        self._create_table_if_not_exist()
+        self.ready = asyncio.Event(loop=loop)
+        self.pool = None
+        asyncio.ensure_future(self.configure(), loop=loop)
 
     @property
     def table_name(self):
         return self.table_info['name']
+
+    async def configure(self):
+        self.pool = await asyncpg.create_pool(**CONF.DATABASE_CREDENTIALS)
+        self._get_table_info()
+        await self._create_table_if_not_exist()
+        self.ready.set()
 
     def _get_table_info(self):
         self.table_info = {
@@ -81,7 +85,7 @@ class DBDriver:
             "columns": {attr: value for attr, value in vars(self.model).items() if isinstance(value, Column)}
         }
 
-    def _create_table_if_not_exist(self):
+    async def _create_table_if_not_exist(self):
         try:
             column_definitions = []
             for column_name, column in self.table_info['columns'].items():
@@ -105,7 +109,7 @@ class DBDriver:
             query = f"CREATE TABLE IF NOT EXISTS {self.table_info['name']}" \
                     f"({', '.join(column_definitions)});"
 
-            self.loop.run_until_complete(self.pool.execute(query))
+            await self.pool.execute(query)
         except exceptions.PostgresError:
             LOG.exception(f"Cannot create table {self.table_name}")
 
