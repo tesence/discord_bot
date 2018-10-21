4c65101a39ad518efc187a46ff257a9c92810e73
tesence <thomasesence@gmail.com>
2018-10-21 21:37:42 +0200

    Implement a cog template for roles

---
+++ discord_bot/cogs/utils/role.py
@@ -0,0 +1,31 @@
+import logging
+
+from discord.ext.commands import converter
+
+from discord_bot import Emoji
+
+
+LOG = logging.getLogger('bot')
+
+
+class RoleCommands:
+
+    def __init__(self):
+        self.role_converter = converter.RoleConverter()
+
+    async def _get_role(self, ctx, role):
+        return await self.role_converter.convert(ctx, role)
+
+    async def add_role(self, ctx, role):
+        role = await self._get_role(ctx, role)
+        if role not in ctx.author.roles:
+            await ctx.author.add_roles(role)
+            await ctx.message.add_reaction(Emoji.WHITE_CHECK_MARK)
+            LOG.debug(f"{ctx.author.name} now has the role: {role.name}")
+
+    async def remove_role(self, ctx, role):
+        role = await self._get_role(ctx, role)
+        if role in ctx.author.roles:
+            await ctx.author.remove_roles(role)
+            await ctx.message.add_reaction(Emoji.WHITE_CHECK_MARK)
+            LOG.debug(f"{ctx.author.name} no longer has the role: {role.name}")
