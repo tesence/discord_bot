33ae9d9ed71de9d7811ac3ac5a03dbde0a7e6064
tesence <thomasesence@gmail.com>
2018-10-31 11:14:08 +0100

    Fix more codacy issues

--- discord_bot/db/stream.py
+++ discord_bot/db/stream.py
@@ -1,10 +1,7 @@
 from datetime import datetime
 import logging
 
-from asyncpg import exceptions
-
 from discord_bot.db import base
-from discord_bot.db.base import transaction
 
 
 LOG = logging.getLogger('bot')
