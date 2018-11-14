4d7d32451f49d20047a0e7df77ebe474301e68fd
tesence <thomasesence@gmail.com>
2018-11-15 00:55:58 +0100

    Update the admin cog

--- gumo/cogs/admin.py
+++ gumo/cogs/admin.py
@@ -2,9 +2,9 @@ import logging
 
 from discord.ext import commands
 
+from gumo import check
 from gumo import config
 from gumo import cogs
-from gumo import utils
 
 LOG = logging.getLogger('bot')
 
@@ -19,20 +19,16 @@ class AdminCommands(cogs.CogMixin):
         return await self.bot.is_owner(ctx.author)
 
     @commands.command()
+    @check.is_owner()
     async def reload(self, ctx, *extensions):
         """Reload some extensions"""
-        channel_repr = utils.get_channel_repr(ctx.channel)
-
         config.load()
 
         for extension in extensions:
             extension = f'cogs.{extension}'
-            if extension in self.bot.extensions:
-                self.bot.unload_extension(extension)
-                self.bot.load_extension(extension)
-                LOG.debug(f"Extension '{extension}' reloaded")
-            else:
-                LOG.warning(f"[{channel_repr}] '{extension}' is not a valid extension")
+            self.bot.unload_extension(extension)
+            self.bot.load_extension(extension)
+            LOG.debug(f"Extension '{extension}' reloaded")
 
 
 def setup(bot):
