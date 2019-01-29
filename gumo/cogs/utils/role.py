ab149c3df22106cdc0317532a26174d8520aec92
tesence <thomasesence@gmail.com>
2019-01-29 16:25:15 +0100

    Refactor the role utils class

--- gumo/cogs/utils/role.py
+++ gumo/cogs/utils/role.py
@@ -1,41 +1,31 @@
 import logging
 
 import discord
-from discord.ext.commands import converter
 
 from gumo import emoji
-
+from gumo import utils
 
 LOG = logging.getLogger('bot')
 
 
 class RoleCommands:
 
-    def __init__(self, *role_names):
-        self.role_converter = converter.RoleConverter()
+    async def _manage_roles(self, ctx, action, *role_names, guild=None):
+        channel_repr = utils.get_channel_repr(ctx.channel)
+        guild = guild or ctx.guild
+        roles = {r: discord.utils.get(guild.roles, name=r) for r in role_names}
+        member = guild.get_member(ctx.author.id)
+
+        for name, role in roles.items():
+            if not isinstance(role, discord.Role):
+                LOG.warning(f"[{channel_repr}] The role '{name}' dis not a valid role in this guild")
+                del roles[name]
+
+        await getattr(member, action)(*roles.values())
+        await ctx.message.add_reaction(emoji.WHITE_CHECK_MARK)
 
     async def add_roles(self, ctx, *role_names, guild=None):
-        guild = guild or ctx.guild
-        for role_name in role_names:
-            await self._add_role(ctx, role_name, guild)
-
-    async def _add_role(self, ctx, role_name, guild):
-        role = discord.utils.get(guild.roles, name=role_name)
-        member = discord.utils.get(guild.members, id=ctx.author.id)
-        if role not in member.roles:
-            await member.add_roles(role)
-            await ctx.message.add_reaction(emoji.WHITE_CHECK_MARK)
-            LOG.debug(f"{ctx.author.name} now has the role '{role.name}' in the guild '{guild.name}'")
+        await self._manage_roles(ctx, 'add_roles', *role_names, guild=None)
 
     async def remove_roles(self, ctx, *role_names, guild=None):
-        guild = guild or ctx.guild
-        for role_name in role_names:
-            await self._remove_role(ctx, role_name, guild)
-
-    async def _remove_role(self, ctx, role_name, guild):
-        role = discord.utils.get(guild.roles, name=role_name)
-        member = discord.utils.get(guild.members, id=ctx.author.id)
-        if role in member.roles:
-            await member.remove_roles(role)
-            await ctx.message.add_reaction(emoji.WHITE_CHECK_MARK)
-            LOG.debug(f"{ctx.author.name} no longer has the role '{role.name}' in the guild '{guild.name}'")
+        await self._manage_roles(ctx, 'remove_roles', *role_names, guild=None)
