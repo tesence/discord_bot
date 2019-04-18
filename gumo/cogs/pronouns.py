26cb312d89fb6198b00761a1d6062e368a9ac8a0
tesence <thomasesence@gmail.com>
2019-04-19 00:54:24 +0200

    Contribute to emoji chains on the Ori discord

--- gumo/cogs/pronouns.py
+++ gumo/cogs/pronouns.py
@@ -1,4 +1,8 @@
+import asyncio
+import collections
 import logging
+import random
+import re
 
 import multidict
 from discord.ext import commands
@@ -7,21 +11,77 @@ from gumo.cogs.utils import role
 
 LOG = logging.getLogger(__name__)
 
+GUILD_ID = 116250700685508615
+ROLES = ["He/Him", "She/Her", "They/Them"]
+EMOJI_REGEX = r'^<a?:[a-zA-Z0-9_]+:([0-9]+)>$'
+
+
+class EmojiChain:
+
+    def __init__(self):
+        self.contributors = set()
+        self.emoji = None
+        self.answered = False
+        self.limit = random.randint(3, 6)
+        self.timeout = random.randint(0, 20)
+
+    def __bool__(self):
+        return bool(self.contributors) and bool(self.emoji)
+
+    def __repr__(self):
+        return f"<emoji={self.emoji} contributors={len(self.contributors)}> answered={self.answered} " \
+               f"limit={self.limit} timeout={self.timeout}>"
+
 
 class PronounRoleCommands(commands.Cog, role.RoleCommands):
 
-    GUILD_ID = 116250700685508615
-    ROLES = ["He/Him", "She/Her", "They/Them"]
-
     def __init__(self, bot):
         super().__init__()
-        self.display_name = "Pronoun roles"
+        self.display_name = "Ori server commands"
         self.bot = bot
-        self.roles = multidict.CIMultiDict(**{role_name: role_name for role_name in self.ROLES})
+        self.roles = multidict.CIMultiDict(**{role_name: role_name for role_name in ROLES})
+        self.emojis_by_channel = collections.defaultdict(EmojiChain)
         self.guild = None
 
+    @commands.Cog.listener()
     async def on_ready(self):
-        self.guild = self.bot.get_guild(self.GUILD_ID)
+        self.guild = self.bot.get_guild(GUILD_ID)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+
+        if message.author.bot or not message.guild.id == GUILD_ID:
+            return
+
+        # Check if the message is a single emoji and retrieve the related guild emoji, None otherwise
+        emoji = None
+        if re.match(EMOJI_REGEX, message.content):
+            emoji = self.bot.get_emoji(int(re.match(EMOJI_REGEX, message.content).group(1)))
+
+        current_chain = self.emojis_by_channel[message.channel]
+
+        # If someone sends a message that is not a single guild emoji, or the wrong one, reset the chain
+        if current_chain and (not emoji or not current_chain.emoji == emoji):
+            LOG.debug(f"{str(message.author)} has reseted the chain {current_chain} with the message "
+                      f"'{message.content}'")
+            del self.emojis_by_channel[message.channel]
+            current_chain = self.emojis_by_channel[message.channel]
+
+        if emoji:
+
+            # If someone starts a chain, use the guild emoji in the message
+            if not current_chain:
+                current_chain.emoji = emoji
+
+            # Add the author as a contributor of the emoji chain
+            current_chain.contributors.add(message.author)
+            LOG.debug(f"{message.author} has contributed to an emoji chain: {current_chain}")
+
+            # If enough people contributed to consider it a emoji chain
+            if len(current_chain.contributors) >= current_chain.limit and not current_chain.answered:
+                current_chain.answered = True
+                await asyncio.sleep(current_chain.timeout)
+                await message.channel.send(current_chain.emoji)
 
     @commands.group(invoke_without_command=True)
     async def pronoun(self, ctx, *pronouns):
