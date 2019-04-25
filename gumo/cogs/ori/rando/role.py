8ad93da3dc1f7351d7b09af8dbd41b3e996005c3
tesence <thomasesence@gmail.com>
2019-04-25 20:47:47 +0200

    Hardcode the rando role

--- gumo/cogs/ori/rando/role.py
+++ gumo/cogs/ori/rando/role.py
@@ -8,6 +8,8 @@ from gumo.cogs.utils import role
 
 LOG = logging.getLogger(__name__)
 
+RANDO_ROLE = "Looking For Rando"
+
 
 class OriRandoRoleCommands(commands.Cog, role.RoleCommands):
 
@@ -17,10 +19,10 @@ class OriRandoRoleCommands(commands.Cog, role.RoleCommands):
         self.rando_role = None
 
     async def __local_check(self, ctx):
-        role = discord.utils.get(ctx.guild.roles, name=config.get('RANDO_ROLE', guild_id=ctx.guild.id))
+        role = discord.utils.get(ctx.guild.roles, name=RANDO_ROLE)
         if not role:
             LOG.warning(f"Extension '{self.__module__}' unavailable for guild '{ctx.guild.name}#{ctx.guild.id}'. "
-                        f"Invalid configuration variable: 'RANDO_ROLE'")
+                        f"Cannot find role: '{RANDO_ROLE}'")
         return role is not None
 
     @commands.group(aliases=['lfg'])
@@ -29,20 +31,18 @@ class OriRandoRoleCommands(commands.Cog, role.RoleCommands):
         """Add/remove the rando role"""
         if ctx.invoked_subcommand is None:
             await ctx.invoke(self.bot.get_command('help'), command_name=ctx.command.name)
-        else:
-            self.rando_role = config.get('RANDO_ROLE', guild_id=ctx.guild.id)
 
     @looking_for_game.command()
     @commands.guild_only()
     async def add(self, ctx):
         """Add the rando role"""
-        await self.add_roles(ctx, self.rando_role, guild=ctx.guild)
+        await self.add_roles(ctx, RANDO_ROLE, guild=ctx.guild)
 
     @looking_for_game.command(aliases=['rm'])
     @commands.guild_only()
     async def remove(self, ctx):
         """Remove the rando role"""
-        await self.remove_roles(ctx, self.rando_role, guild=ctx.guild)
+        await self.remove_roles(ctx, RANDO_ROLE, guild=ctx.guild)
 
 
 def setup(bot):
