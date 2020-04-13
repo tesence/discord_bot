0b276707ff6848fc84b14d2d318d81067cb0648d
tesence <thomasesence@gmail.com>
2020-04-13 15:58:48 +0200

    Remove test stream check as it's never used

--- gumo/cogs/stream/core.py
+++ gumo/cogs/stream/core.py
@@ -93,11 +93,6 @@ class StreamCommands(commands.Cog):
             game_data = await self.client.get_games(game_id)
             game_name = game_data[0]['name'].strip() if game_data else None
 
-            # Treat it as a test stream if there is no game set and if there is word "test" in the title
-            if "test" in stream_data['title'].strip().lower() and not game_name:
-                LOG.info(f"'{stream.name}' is doing a test stream, the notification is not sent")
-                return
-
             # Update old values
             stream.name = current_name
             stream.display_name = user_data['display_name']
