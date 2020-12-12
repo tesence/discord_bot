aae555dcdfde6f69669f264523c1a4c2f691053a
tesence <thomasesence@gmail.com>
2020-12-12 17:06:37 +0100

    Remove old rank roles when a user acquire a new one

--- gumo/cogs/guilds/julgane.py
+++ gumo/cogs/guilds/julgane.py
@@ -1,33 +1,39 @@
 import logging
 
-import discord
 from discord.ext import commands
-
-from gumo.cogs.utils import role
 
 LOG = logging.getLogger(__name__)
 
 GUILD_ID = 356253023275581442
-PODE_ROLE = 356253780678934532
+
+ROLES = {
+    'graine': 356253780678934532,
+    'germe': 748493294845034570,
+    'pomme de terre': 748493403829960784,
+    'frites': 748493454040104962,
+    'poutine': 748493495466983424,
+    'ohpuree': 748493539402448927,
+    'tartiflette': 748493586454151248,
+    'raclette': 748493631702302771,
+}
 
 
-class JulganeGuildCommands(commands.Cog, role.RoleCommands):
+class JulganeGuildCommands(commands.Cog):
 
     def __init__(self, bot):
         self.display_name = "Julgane guild commands"
         self.bot = bot
         self.guild = None
-        self.pode_role = None
+        self.roles = None
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.guild = self.guild or self.bot.get_guild(GUILD_ID)
-        self.pode_role = self.pode_role or self.guild.get_role(PODE_ROLE)
+        self.roles = {name: self.guild.get_role(id) for name, id in ROLES.items()}
         for member in self.guild.members:
-            if not self.pode_role in member.roles:
-                await member.add_roles(self.pode_role)
-                LOG.debug(f"The role '{self.pode_role.name}' has been given to '{member.name}'")
-
+            if self.roles['graine'] not in member.roles:
+                await member.add_roles(self.roles['graine'])
+                LOG.debug(f"The role '{self.roles['graine'].name}' has been given to '{member.name}'")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -35,9 +41,29 @@ class JulganeGuildCommands(commands.Cog, role.RoleCommands):
         if not member.guild.id == GUILD_ID:
             return
 
-        self.pode_role = member.guild.get_role(PODE_ROLE)
-        await member.add_roles(self.pode_role)
-        LOG.debug(f"'{member.name}' just joined, the role '{self.pode_role.name}' has been given")
+        await member.add_roles(self.roles['graine'])
+        LOG.debug(f"'{member.name}' just joined, the role '{self.roles['graine'].name}' has been given")
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+
+        if not after.guild.id == GUILD_ID or before.roles == after.roles:
+            return
+
+        added_rank_roles = [role for role in after.roles if role not in before.roles and
+                            role in self.roles.values() and not role == self.roles['graine']]
+
+        if not added_rank_roles or added_rank_roles == [self.roles['graine']]:
+            return
+
+        old_rank_roles = [role for role in after.roles if role in self.roles.values()
+                          and role not in added_rank_roles]
+
+        await after.remove_roles(*old_rank_roles)
+
+        LOG.debug(f"'{after.name}' acquired the roles {', '.join([role.name for role in added_rank_roles])}, "
+                  f"the roles {', '.join([role.name for role in old_rank_roles])}' have been removed")
+
 
 def setup(bot):
     bot.add_cog(JulganeGuildCommands(bot))
